54c782ee4ddfaacca3026a40f4a506903980b7d7
JermainLiu <2223860593@qq.com>
2023-10-02 00:34:08 +0800

    Site updated: 2023-10-02 00:34:01

--- js/anzhiyu.js
+++ js/anzhiyu.js
@@ -6,7 +6,7 @@ var anzhiyu = {
     if (window.location.pathname != "/music/") {
       return;
     }
-    const anMusicBg = document.getElementById("an_music_bg")
+    const anMusicBg = document.getElementById("an_music_bg");
 
     if (isChangeBg) {
       // player listswitch 会进入此处
@@ -14,44 +14,47 @@ var anzhiyu = {
       anMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
     } else {
       // 第一次进入，绑定事件，改背景
-      let timer = setInterval(()=&gt;{
+      let timer = setInterval(() =&gt; {
         const musiccover = document.querySelector("#anMusic-page .aplayer-pic");
         // 确保player加载完成
         console.info(anMusicBg);
         if (musiccover) {
-          clearInterval(timer)
+          clearInterval(timer);
           anMusicBg.style.backgroundImage = musiccover.style.backgroundImage;
           // 绑定事件
           anzhiyu.addEventListenerChangeMusicBg();
-          
+
           // 暂停nav的音乐
-          if (document.querySelector("#nav-music meting-js").aplayer &amp;&amp; !document.querySelector("#nav-music meting-js").aplayer.audio.paused){
-            anzhiyu.musicToggle()
+          if (
+            document.querySelector("#nav-music meting-js").aplayer &amp;&amp;
+            !document.querySelector("#nav-music meting-js").aplayer.audio.paused
+          ) {
+            anzhiyu.musicToggle();
           }
         }
-      }, 100)
+      }, 100);
     }
   },
   addEventListenerChangeMusicBg: function () {
     const anMusicPage = document.getElementById("anMusic-page");
     const aplayerIconMenu = anMusicPage.querySelector(".aplayer-info .aplayer-time .aplayer-icon-menu");
 
-    anMusicPage.querySelector("meting-js").aplayer.on('loadeddata', function () {
+    anMusicPage.querySelector("meting-js").aplayer.on("loadeddata", function () {
       anzhiyu.changeMusicBg();
-      console.info('player loadeddata');
+      console.info("player loadeddata");
     });
 
     aplayerIconMenu.addEventListener("click", function () {
-      document.getElementById('menu-mask').style.display = "block";
-      document.getElementById('menu-mask').style.animation = "0.5s ease 0s 1 normal none running to_show";
-    })
+      document.getElementById("menu-mask").style.display = "block";
+      document.getElementById("menu-mask").style.animation = "0.5s ease 0s 1 normal none running to_show";
+    });
 
-    document.getElementById('menu-mask').addEventListener("click", function () {
+    document.getElementById("menu-mask").addEventListener("click", function () {
       if (window.location.pathname != "/music/") return;
-      anMusicPage.querySelector('.aplayer-list').classList.remove("aplayer-list-hide");
-    })
+      anMusicPage.querySelector(".aplayer-list").classList.remove("aplayer-list-hide");
+    });
   },
-}
+};
 
 // 调用
 anzhiyu.changeMusicBg(false);
